6a445dfac3ef3ba0f199faa59ebcb6f203452454
v1b3m <vibenjamin6@gmail.com>
2018-07-05 21:53:40 +0300

    Add time functions

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,7 +18,7 @@ void error(const char *msg)
 void reverse(char s[]);
 void dbl(char s[]);
 void del(char word[],int i);
-void strreplace(char s[], int i, char repl_chr);
+
 
 //int argc is the total number of parameters we'll be passing
 int main(int argc, char *argv[])
@@ -61,8 +62,16 @@ int main(int argc, char *argv[])
 
   while(1)
   {
+    FILE * fp;
+    fp = fopen("log.txt","w");
+    fprintf(fp,"We're up");
     char message[255];
     char* word;
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,
+    tm.tm_min, tm.tm_sec);
 
     bzero(buffer, 255);
     n = read(newsockfd, buffer, 255);
@@ -77,20 +86,28 @@ int main(int argc, char *argv[])
     int a;
     if(strncmp("rev",message,3) == 0)
     {
+        clock_t begin = clock();
         word = strtok(NULL," ");
         reverse(word);
+        clock_t end = clock();
+        double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+        printf("%f\n",time_spent );
         n = write(newsockfd, word ,strlen(word));
         if (n < 0 )
     			error("Error on writing");
         bzero(word,sizeof(word));
     } else if (strncmp("dbl",message,3) == 0)
     {
+      clock_t begin = clock();
       word = strtok(NULL," ");
       dbl(word);
+      clock_t end = clock();
       write(newsockfd, word ,strlen(word));
+      double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
       bzero(word,sizeof(word));
     } else if (strncmp("del",message,3) == 0)
     {
+      clock_t begin = clock();
       char word[50];
       char numbers[20];
 
@@ -130,21 +147,45 @@ int main(int argc, char *argv[])
         j++;
       }
     }
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
       write(newsockfd, word ,sizeof(word));
     } else if (strncmp("rep",message,3) == 0)
     {
-      char s[25];
-      strcpy(s,strtok(NULL," "));
-      printf("%s",s);
+      char word[50];
+      char replacements[50];
+
+      int index;
+      char letter[1];
+
+
+
+      strcpy(word,strtok(NULL," "));
+      strcpy(replacements,strtok(NULL," "));
+
+      index = atoi(strtok(replacements,"-"));
+      strcpy(letter,strtok(NULL,"-"));
+
+      char* p = word;
+      p[index] = letter;
+
+      //replace the character
+
+      write(newsockfd, p ,sizeof(p));
+      bzero(word, 50);
+      bzero(replacements,50);
+      bzero(letter,1);
     }
 
-  //reads bytes in a string into the buffer pointed to by the first parameter
-  //    fgets(buffer , 255 ,stdin);
+    //reads bytes in a string into the buffer pointed to by the first parameter
+    //    fgets(buffer , 255 ,stdin);
     int i = strncmp("Bye" , buffer , 3);
     if(i == 0)
     break;
+    fclose(fp);
   }
+  
   close(newsockfd);
   close(sockfd);
   return 0;
@@ -184,9 +225,4 @@ void dbl(char s[]) {
 
 void del(char word[],int i) {
     memmove(&word[i], &word[i + 1], strlen(word) - i);
-}
-
-void strreplace(char s[], int i, char repl_chr)
-{
-  s[i]=repl_chr;
 }
