540c14cec4debb066a4b684324c3c275851a7074
v1b3m <vibenjamin6@gmail.com>
2018-07-23 12:29:20 +0300

    Update server

--- server.c
+++ server.c
@@ -168,7 +168,7 @@ void *connection_handler(void *socket_desc)
           double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
           strcpy(word,word1);
 
-            fprintf(fp,"%d,rev,%f,%d-%d-%d %d:%d:%d\n",userId, time_spent,tm.tm_year + 1900,
+            fprintf(fp,"%d,reverse,%f,%d-%d-%d %d:%d:%d\n",userId, time_spent,tm.tm_year + 1900,
             tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,tm.tm_min, tm.tm_sec);
         } else {
           strcpy(word,error);
@@ -242,7 +242,7 @@ void *connection_handler(void *socket_desc)
       strcpy(word,word1);
 
         double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-        fprintf(fp,"%d,del,%f,%d-%d-%d %d:%d:%d\n",userId, time_spent,tm.tm_year + 1900,
+        fprintf(fp,"%d,delete,%f,%d-%d-%d %d:%d:%d\n",userId, time_spent,tm.tm_year + 1900,
         tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,tm.tm_min, tm.tm_sec);
       } else {
         strcpy(word,error);
@@ -339,7 +339,6 @@ void *connection_handler(void *socket_desc)
           if (strlen(word1) <= 50 )
           {
           clock_t begin = clock();
-          int userId = rand();
 
           int i, j, temp;
           int l = strlen(word1);
@@ -361,7 +360,7 @@ void *connection_handler(void *socket_desc)
             strcat(word,",");
             trimwhitespace(word);
 
-            fprintf(fp,"%d,rev,%f,%d-%d-%d %d:%d:%d\n",userId, time_spent,tm.tm_year + 1900,
+            fprintf(fp,"%d,reverse,%f,%d-%d-%d %d:%d:%d\n",userId, time_spent,tm.tm_year + 1900,
             tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,tm.tm_min, tm.tm_sec);
           } else {
             strcat(word,job);
@@ -376,7 +375,6 @@ void *connection_handler(void *socket_desc)
           char* job1;
           // strcpy(job,strtok(NULL," "));
           clock_t begin = clock();
-          int userId = rand();
           job1 = strtok(NULL," ");
           if(strlen(job1) <= 50 ) {
           dbl(job1);
@@ -406,7 +404,6 @@ void *connection_handler(void *socket_desc)
           if (strlen(word1) <= 50)
           {
           clock_t begin = clock();
-          int userId = rand();
           // char word[50];
           char numbers[20];
 
@@ -455,7 +452,7 @@ void *connection_handler(void *socket_desc)
         trimwhitespace(word);
 
           double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-          fprintf(fp,"%d,del,%f,%d-%d-%d %d:%d:%d\n",userId, time_spent,tm.tm_year + 1900,
+          fprintf(fp,"%d,delete,%f,%d-%d-%d %d:%d:%d\n",userId, time_spent,tm.tm_year + 1900,
           tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,tm.tm_min, tm.tm_sec);
         } else {
           strcat(word,job);
@@ -469,7 +466,12 @@ void *connection_handler(void *socket_desc)
         {
           //some wrork to be done here
           char * message = "Unfortunately, we're unable to process this request at the moment!";
-          strcpy(word,message);
+          strcat(word,job);
+          trimwhitespace(word);
+          strcat(word,": ");
+          strcat(word,message);
+          strcat(word,",");
+          trimwhitespace(word);
 
         } else if (strncmp("enc",type,3) == 0) {
           char word1[256];
@@ -477,7 +479,6 @@ void *connection_handler(void *socket_desc)
           if (strlen(word1) <= 50)
           {
           clock_t begin = clock();
-          int userId = rand();
 
           char key[256];
 
@@ -511,7 +512,6 @@ void *connection_handler(void *socket_desc)
           strcpy(word1,strtok(NULL," "));
           if (strlen(word1) <= 50) {
             clock_t begin = clock();
-            int userId = rand();
 
             char key[10];
             trimwhitespace(word1);
